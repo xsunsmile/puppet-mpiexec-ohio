d83bd9bf362367371a31c05488678b3d16e5b022
hao.sun <xsunsmile@gmail.com>
2011-10-18 17:33:45 +0900

    update

--- files/fetch.sh
+++ files/fetch.sh
@@ -4,8 +4,9 @@
 repo_url="http://www.osc.edu/~djohnson/mpiexec"
 wget -t 1 --connect-timeout=10 $repo_url/mpiexec-$version.tgz
 if [ -e mpiexec-$version.tgz ]; then
+  [ -e mpiexec ] && rm -rf mpiexec
   tar zxf mpiexec-$version.tgz 2>&1 >/dev/null
-  # rm -rf mpiexec-$version.tgz
+  rm -rf mpiexec-$version.tgz
   mv mpiexec-$version mpiexec
 fi
 [ ! -e mpiexec ] && git clone https://github.com/xsunsmile/mpiexec-clone.git mpiexec
--- manifests/compile.pp
+++ manifests/compile.pp
@@ -20,21 +20,21 @@ class mpiexec::compile {
 
 	exec { "download-mpiexec":
 		cwd => "${mpiexec::params::install_src}",
-		command => "/bin/sh fetch.sh",
-		onlyif => "test ! -e ${mpiexec::params::install_src}/mpiexec",
+		command => "/bin/sh fetch.sh ${mpiexec::params::mpiexec_version}",
+		timeout => 600,
 		require => File["${mpiexec::params::install_src}/fetch.sh"],
 	}
 
 	file { "${mpiexec::params::install_src}/mpiexec":
 		ensure => directory,
 		mode => 0777,
-		require => Exec['download'],
+		require => Exec['download-mpiexec'],
 	}
 
 	exec { "configure-mpiexec":
-		path => "/bin:/usr/bin:/usr/sbin",
+		path => "/bin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin",
 		cwd => "${mpiexec::params::install_src}/mpiexec",
-		command => "nice -19 sh configure ${mpiexec::params::compile_args}",
+		command => "nice -19 sh configure ${mpiexec::params::compile_args} 2>&1 | tee -a compile.log",
 		require => [ File["${mpiexec::params::install_src}/mpiexec"], Package['build-essential'] ],
 		onlyif => "test ! -e ${mpiexec::params::install_src}/mpiexec/config.log",
 	}
@@ -44,8 +44,6 @@ class mpiexec::compile {
 		cwd => "${mpiexec::params::install_src}/mpiexec",
 		command => "nice -19 make",
 		require => Exec['configure-mpiexec'],
-		timeout => 0,
-		onlyif => "test ! -e ${mpiexec::params::spool_dir}"
 	}
 
 	fpm::mpiexec{ 'mpiexec':
@@ -53,7 +51,7 @@ class mpiexec::compile {
 		package_type => 'deb',
 		package_src => "${mpiexec::params::install_src}/mpiexec",
 		package_version => "${mpiexec::params::mpiexec_version}",
-		build_dirname => '/tmp/build',
+		build_dirname => '/tmp/build_mpiexec',
 		broker_dir => '/etc/puppet/modules/mpiexec/files',
 		repo => "mongodb://${mongodb_host}:27017/inters_debs/${architecture}/_/tmp/mpiexec*.deb",
 	}
