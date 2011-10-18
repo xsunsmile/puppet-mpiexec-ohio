a5a36400aff1bf6c42d83ab480d8189d6fb5b7ad
Hao SUN <xsunsmile@gmail.com>
2011-10-18 15:38:14 +0900

    add version for fetch

--- files/fetch.sh
+++ files/fetch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-version="0.84"
+[ ! -z "$1" ] && version="$1" || version="0.84"
 repo_url="http://www.osc.edu/~djohnson/mpiexec"
 wget -t 1 --connect-timeout=10 $repo_url/mpiexec-$version.tgz
 if [ -e mpiexec-$version.tgz ]; then
   tar zxf mpiexec-$version.tgz 2>&1 >/dev/null
-  rm -rf mpiexec-$version.tgz
+  # rm -rf mpiexec-$version.tgz
   mv mpiexec-$version mpiexec
 fi
 [ ! -e mpiexec ] && git clone https://github.com/xsunsmile/mpiexec-clone.git mpiexec
